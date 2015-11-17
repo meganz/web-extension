4feddef8677706b1618375c19fa72f69a1b33ead
Diego Casorran <dcasorran@gmail.com>
2015-11-17 12:28:50 +0100

    Prevent redirection for *.deb & *.rpm packages.

--- mega.js
+++ mega.js
@@ -1,30 +1,39 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) 
-	{
-		if ((details.url.substr(-4) == '.xml')
-			|| (details.url.substr(-4) == '.crx')
-			|| (details.url.substr(-4) == '.xpi')
-			|| (details.url.substr(-4) == '.exe') || (details.url.substr(-4) == '.dmg') || (details.url.substr(-3) == '.gz') || (details.url.substr(-4) == '.zip') || (details.url.substr(-3) == '.js')) return { cancel:  false };	
-		else
-		{
-			var hash = '';
-			if (details.url.indexOf('#') > -1) hash = '#' + details.url.split('#')[1];			
-			return { redirectUrl:  chrome.extension.getURL("mega/secure.html" + hash) };
-		}
+    function(details)
+    {
+        if ((details.url.substr(-4) == '.xml')
+            || (details.url.substr(-4) == '.crx')
+            || (details.url.substr(-4) == '.xpi')
+            || (details.url.substr(-4) == '.exe')
+            || (details.url.substr(-4) == '.dmg')
+            || (details.url.substr(-3) == '.gz')
+            || (details.url.substr(-3) == '.deb')
+            || (details.url.substr(-3) == '.rpm')
+            || (details.url.substr(-4) == '.zip')
+            || (details.url.substr(-3) == '.js')) {
+
+                return { cancel:  false };
+        }
+        else
+        {
+            var hash = '';
+            if (details.url.indexOf('#') > -1) hash = '#' + details.url.split('#')[1];
+            return { redirectUrl:  chrome.extension.getURL("mega/secure.html" + hash) };
+        }
     },
     {
         urls: [
-			"http://mega.co.nz/*",
+            "http://mega.co.nz/*",
             "https://mega.co.nz/*",
-			"http://www.mega.co.nz/*",
+            "http://www.mega.co.nz/*",
             "https://www.mega.co.nz/*",
             "http://mega.nz/*",
             "https://mega.nz/*",
-			"http://www.mega.nz/*",
+            "http://www.mega.nz/*",
             "https://www.mega.nz/*",
-			"http://mega.is/*",
+            "http://mega.is/*",
             "https://mega.is/*",
-			"http://www.mega.is/*",
+            "http://www.mega.is/*",
             "https://www.mega.is/*"
         ],
         types: ["main_frame","sub_frame"]
@@ -33,9 +42,9 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 chrome.webRequest.onHeadersReceived.addListener(
-  function(details) 
-	{	
-		console.log('responseHeaders',responseHeaders);
+  function(details)
+    {
+        console.log('responseHeaders',responseHeaders);
     },
     {
         urls: [
